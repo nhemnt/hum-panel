d2a2e1c2cc445cff7921d7740db584f44f0cfe18
hemant <nhemnt@gmail.com>
2018-12-07 00:14:54 +0530

    react-auth-guard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './store' 
+import store from './store';
+import { UserIsAuthenticated, UserIsNotAuthenticated } from './helpers/auth'
 
 import AppNavbar from './components/layout/AppNavbar'
 import Dashboard from './components/layout/Dashboard'
@@ -19,11 +20,11 @@ class App extends Component {
             <AppNavbar />
             <div className="container mt-2">
               <Switch>
-                <Route exact path="/" component={ Dashboard } />
-                <Route exact path="/client/add" component={ AddClient } />
-                <Route exact path="/client/:id" component={ ClientDetails } />
-                <Route exact path="/client/edit/:id" component={ EditClient } />
-                <Route exact path="/login" component={ Login } />
+                <Route exact path="/" component={ UserIsAuthenticated(Dashboard) } />
+                <Route exact path="/client/add" component={ UserIsAuthenticated(AddClient) } />
+                <Route exact path="/client/:id" component={ UserIsAuthenticated(ClientDetails) } />
+                <Route exact path="/client/edit/:id" component={ UserIsAuthenticated(EditClient) } />
+                <Route exact path="/login" component={ UserIsNotAuthenticated(Login) } />
               </Switch>
             </div>
           </div>
